13fe5f2e576e3c3b3d6b5ad3dd96cb9eb8a4227b
John Wiltshire <wiltshire.john@gmail.com>
2016-05-16 13:07:29 +0100

    Added Layout

--- app/src/main/java/com/example/wiltshirej/jwstepcounter/DatabaseHelper.java
+++ app/src/main/java/com/example/wiltshirej/jwstepcounter/DatabaseHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.IntegerRes;
 
 /**
  * Created by wiltshirej on 13/05/16.
@@ -60,6 +61,51 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         Cursor res = db.rawQuery("SELECT * FROM " + TABLE_NAME,null);
         return res;
+
+    }
+
+    public boolean updateData (String id, String name, String surname, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, surname);
+        contentValues.put(COL_4, marks);
+
+        int result = db.update(TABLE_NAME, contentValues,COL_1+" = ?",new String[] {id});
+
+        if(result ==0 )
+            return false;
+        else
+            return true;
+
+
+    }
+
+    public boolean deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        int result = db.delete(TABLE_NAME,COL_1+"=?", new String[] {id});
+
+        if(result ==0 )
+            return false;
+        else
+            return true;
+
+
+    }
+
+    public boolean deleteAllData(){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        int result = db.delete(TABLE_NAME,null,null);
+
+        if(result ==0 )
+            return false;
+        else
+            return true;
 
     }
 }
--- app/src/main/java/com/example/wiltshirej/jwstepcounter/MainActivity.java
+++ app/src/main/java/com/example/wiltshirej/jwstepcounter/MainActivity.java
@@ -1,81 +1,99 @@
 package com.example.wiltshirej.jwstepcounter;
 
+import android.content.Context;
 import android.database.Cursor;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     DatabaseHelper myDB;
-    EditText editName, editSurname, editMarks;
-    Button btnAddData, btnViewAll;
+
+    EditText editName, editSurname, editMarks, editId;
+    Button btnAddData, btnViewAll, btnViewUpdate, btnDelete, btnDeleteAll, btnUpdateSteps;
+    TextView editSteps;
+
+    private SensorManager mSensorManager;
+    private Sensor mStepCounterSensor;
+    private Sensor mStepDetectorSensor;
+
+    boolean activityRunning;
+    public boolean active = false;
+    private SensorManager sensorManager;
+    private Sensor stepDetectorSensor;
+    public int stepCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d("Debug","Starting main onCreate");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         myDB = new DatabaseHelper(this);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        editName = (EditText) findViewById(R.id.editText_name);
-        editSurname = (EditText) findViewById(R.id.editText_surname);
-        editMarks = (EditText) findViewById(R.id.editText_marks);
 
-        btnAddData = (Button) findViewById(R.id.button_add);
-        btnViewAll = (Button) findViewById(R.id.button_viewAll);
+        //editName = (EditText) findViewById(R.id.editText_name);
+        //editSurname = (EditText) findViewById(R.id.editText_surname);
+        //editMarks = (EditText) findViewById(R.id.editText_marks);
+        //editId = (EditText) findViewById(R.id.editText_id);
+        editSteps = (TextView) findViewById(R.id.editText_steps);
 
-        addData();
-        viewAll();
+
+
+        //btnAddData = (Button) findViewById(R.id.button_add);
+        //btnViewAll = (Button) findViewById(R.id.button_viewAll);
+        //btnViewUpdate = (Button) findViewById(R.id.button_update);
+        //btnDelete = (Button) findViewById(R.id.button_delete);
+        //btnDeleteAll = (Button) findViewById(R.id.button_deleteAll);
+        btnUpdateSteps = (Button) findViewById(R.id.button_updateSteps);
+
+        updateSteps();
 
     }
 
-    public void addData(){
-        btnAddData.setOnClickListener(
+
+
+    public void updateSteps(){
+        btnUpdateSteps.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        boolean isInserted = myDB.insertData(editName.getText().toString(),editSurname.getText().toString(),editMarks.getText().toString());
+                        //editSteps.setText("99");
+                        if(!active){
+                            sensorManager.registerListener(MainActivity.this, stepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                            btnUpdateSteps.setText("Stop");
+                            active=true;
+                        }
+                        else{
+                            sensorManager.unregisterListener(MainActivity.this, stepDetectorSensor);
+                            btnUpdateSteps.setText("Start");
+                            active=false;
 
-                        if(isInserted)
-                            Toast.makeText(MainActivity.this,"Data Inserted", Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(MainActivity.this,"Data Not Inserted", Toast.LENGTH_LONG).show();
+                        }
+
+
+
                     }
                 }
         );
     }
 
-    public void viewAll(){
-        btnViewAll.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Cursor res = myDB.getAllData();
-                        if(res.getCount() == 0){
-                            //Show no data message
-                            showMessage("Error", "No Data Found");
-                            return;
-                        }
 
-                        StringBuffer buffer = new StringBuffer();
-                        while (res.moveToNext()){
-                            buffer.append("ID: " + res.getString(0) + "\n");
-                            buffer.append("Name: " + res.getString(1) + "\n");
-                            buffer.append("Surname: " + res.getString(2) + "\n");
-                            buffer.append("Marks: " + res.getString(3) + "\n\n");
-                        }
-
-                        showMessage("Data", buffer.toString());
-                    }
-                }
-        );
-    }
 
     public void showMessage(String title, String message){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -85,4 +103,55 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
 
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Count.open();
+        activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    //   private String getDateTime() {
+    //     SimpleDateFormat dateFormat = new SimpleDateFormat(
+    //            "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+    //    Date date = new Date();
+    //    return dateFormat.format(date);
+    //  }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Count.close();
+        activityRunning = false;
+        // if you unregister the last listener, the hardware will stop detecting step events
+        //sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (activityRunning & (active == true)) {
+            stepCount++;
+            //Countint = stepCount ;
+            editSteps.setText(Integer.toString(stepCount));
+            // Total_Count.setText(String.valueOf(event.values[0]));
+        }
+
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+
 }
